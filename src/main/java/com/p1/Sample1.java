67d4bfd0b35f4086dbf0593a9f285419950a8e4f
Manisha <pimplemp@rediffmail.com>
2024-05-28 20:54:52 +0530

    Commit1

---
+++ src/main/java/com/p1/Sample1.java
@@ -0,0 +1,11 @@
+package com.p1;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+
+	}
+
+}
---
+++ src/test/java/com/test/Test1.java
@@ -0,0 +1,18 @@
+package com.test;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Test.*;
+
+public class Test1 {
+	@Test
+	public void testName()
+	{
+		String name="manisha";
+		String name1="manisha";
+		assertEquals(name, name1);
+	}
+}
